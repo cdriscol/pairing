ea325d61b755f2995053558220b033a6f3f33e8d
Chris Driscol <drizkol@gmail.com>
2017-03-23 12:46:35 -0600

    Removing directions from service file.

--- controllers/user-activities.js
+++
@@ -1,5 +0,0 @@
-// - should always return activity name, id, owner id, owner name
-// - returns [{activityName, activityId, ownerName, ownerId}]
-// - getActivityByIdAsync(actId)
-// - getActivitiesByOwnersAsync(userIds)
-// - getActivitiesByOwnerAsync(userId)
